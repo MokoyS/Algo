b013518e5d47b5e2169a47de686bc07f82acdbba
Maxime Lebas <maxandco2003@gmail.com>
2023-10-11 08:59:27 +0200

    INIT

---
+++ Exercice/class.js
@@ -0,0 +1,19 @@
+// EXERCICE 1
+
+class Pokemon {
+    constructor (name, attack, defense, hp, luck) {
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+    }
+
+    isLucky {
+        
+    }
+
+    attackPokemon {
+
+    }
+}
